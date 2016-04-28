35aad1777b5cf0d7ca44bbd520d74a77689d658f
sawanoboly <sawanoboriyu@higanworks.com>
2016-04-28 18:11:40 +0900

    update welcome message

--- lambda_function.py
+++ lambda_function.py
@@ -97,13 +97,13 @@ def get_welcome_response():
 
     session_attributes = {}
     card_title = "Welcome"
-    speech_output = "Welcome to the Alexa Skills Kit sample. " \
-                    "Please tell me your favorite color by saying, " \
-                    "my favorite color is red"
+    speech_output = "Hi, my name is AMIMOTO Ninja." \
+            "Please tell me your favorite color by saying, " \
+            "i am John"
     # If the user either does not reply to the welcome message or says something
     # that is not understood, they will be prompted again with this text.
     reprompt_text = "Please tell me your favorite color by saying, " \
-                    "my favorite color is red."
+            "i am John"
     should_end_session = False
     return build_response(session_attributes, build_speechlet_response(
         card_title, speech_output, reprompt_text, should_end_session))
