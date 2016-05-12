aac2740ee3c971ffd300d2d32c1ec32ac17a9d93
sawanoboly <sawanoboriyu@higanworks.com>
2016-05-12 18:52:01 +0900

    dispatch_no_intent

--- lambda_function.py
+++ lambda_function.py
@@ -199,6 +199,28 @@ def dispatch_yes_intent(intent, session):
     return build_response(session_attributes, build_speechlet_response(
         card_title, speech_output, None, should_end_session))
 
+def dispatch_no_intent(intent, session):
+    """Dispatch no intent and tweet. Then end session.
+    """
+
+    card_title = "No"
+    session_attributes = build_session_attributes(session)
+#    text_data = load_text_from_yaml(card_title)
+    debug_logger(session)
+
+    if session_attributes['state'] in ['on_question']:
+        # WIP ask impression and Tweet
+        speech_output = "Thank you for trying the, A MI MO TO Ninja. " \
+                        "Have a nice day! "
+        should_end_session = False
+    else:
+        speech_output = "Thank you for trying the, A MI MO TO Ninja. " \
+                        "Have a nice day! "
+        should_end_session = True
+
+    return build_response({}, build_speechlet_response(
+        card_title, speech_output, None, should_end_session))
+
 def set_visitor_name_from_session(intent, session):
     """ Sets the visitor name in the session and prepares the speech to reply to the
     user.
