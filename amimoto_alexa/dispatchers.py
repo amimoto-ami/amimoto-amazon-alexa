40d4e1d7582578f115e85ae38ca3a58e59c3ae7d
sawanoboly <sawanoboriyu@higanworks.com>
2016-05-18 16:22:21 +0900

    flake8

--- amimoto_alexa/dispatchers.py
+++ amimoto_alexa/dispatchers.py
@@ -77,5 +77,5 @@ def dispatch_no_intent(intent, session):
                         "Have a nice day! "
         should_end_session = True
 
-    return build_response(session_attributes build_speechlet_response(
+    return build_response(session_attributes, build_speechlet_response(
         card_title, speech_output, None, should_end_session))
--- amimoto_alexa/wpapi.py
+++ amimoto_alexa/wpapi.py
@@ -14,24 +14,24 @@ import urllib2
 import yaml
 yaml_path = 'data/WPConf.yml'
 
-def create_query( author_name, comment ):
-	params = {
-		'content': comment,
-		'author_name': author_name
-	}
-	return params;
 
-def post_comment( url, params ):
-	api_url = url + '/wp-json/wp/v2/comments/';
-	params = urllib.urlencode( params )
-	req = urllib2.Request( api_url )
-	req.add_data( params )
-	res = urllib2.urlopen( req )
-	body = res.read()
+def create_query(author_name, comment):
+    params = {'content': comment,
+              'author_name': author_name}
+    return params
 
-def comment_to_wordpress( author_name, comment ):
-	conf = yaml.load( open( yaml_path, 'r' ) )
-	query = create_query( author_name, comment  )
-	query['post'] = conf['wordpress']['post']
-	url = conf['wordpress']['url']
-	post_comment( url, query )
+
+def post_comment(url, params):
+    api_url = url + '/wp-json/wp/v2/comments/'
+    params = urllib.urlencode(params)
+    req = urllib2.Request(api_url)
+    req.add_data(params)
+    return urllib2.urlopen(req)
+
+
+def comment_to_wordpress(author_name, comment):
+    conf = yaml.load(open(yaml_path, 'r'))
+    query = create_query(author_name, comment)
+    query['post'] = conf['wordpress']['post']
+    url = conf['wordpress']['url']
+    post_comment(url, query)
