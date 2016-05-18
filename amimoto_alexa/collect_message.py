7eb95eeb8eed40893193fcb615e25a034c4a4fa3
sawanoboly <sawanoboriyu@higanworks.com>
2016-05-18 15:58:28 +0900

    remove dup key

--- amimoto_alexa/collect_message.py
+++ amimoto_alexa/collect_message.py
@@ -26,7 +26,7 @@ def collect_impression(intent, session):
 # todo: tweet if exist id.
 # todo: store session summary to firehose
 
-    comment_to_wordpress(session_attributes['attributes']['VisitorName'], impression)
+    comment_to_wordpress(session_attributes['VisitorName'], impression)
     should_end_session = True
     return build_response(session_attributes, build_speechlet_response(
         card_title, speech_output, None, should_end_session))
