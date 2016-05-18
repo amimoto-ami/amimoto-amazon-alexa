6e4433bd5dc7cc01857f7eb0bdfb9297e0110872
sawanoboly <sawanoboriyu@higanworks.com>
2016-05-18 16:00:30 +0900

    check user id before end actions

--- amimoto_alexa/collect_message.py
+++ amimoto_alexa/collect_message.py
@@ -25,8 +25,12 @@ def collect_impression(intent, session):
 
 # todo: tweet if exist id.
 # todo: store session summary to firehose
+    debug_logger(session['user']['userId'])
 
-    comment_to_wordpress(session_attributes['VisitorName'], impression)
+    # check right user?
+    if lamvery.secret.get('DC_ID') == session['user']['userId']
+        comment_to_wordpress(session_attributes['VisitorName'], impression)
+
     should_end_session = True
     return build_response(session_attributes, build_speechlet_response(
         card_title, speech_output, None, should_end_session))
