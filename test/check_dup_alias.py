7058c53b270af39194b755a29c29bbb6b05f44a2
sawanoboly <sawanoboriyu@higanworks.com>
2016-05-24 18:03:15 +0900

    show dep words

--- test/check_dup_alias.py
+++ test/check_dup_alias.py
@@ -7,6 +7,7 @@ helpers for amimoto_alexa
 from __future__ import print_function
 from __future__ import unicode_literals
 import yaml
+from collections import Counter
 
 flatten=lambda i:[a for b in i for a in(flatten(b)if hasattr(b,'__iter__')else(b,))]
 
@@ -17,5 +18,5 @@ with open('data/aliases.yml') as f:
 if len(words) == len(set(words)):
     print("No dup aliases")
 else:
-    print(words)
+    print([words for key,val in Counter(a).items() if val > 2])
     raise StandardError, "found dup aliases!!"
